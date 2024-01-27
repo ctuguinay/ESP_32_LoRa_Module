997899d712c1b9a6d6e6e852f1c64d7a5b040c2f
Caesar Tuguinay <cmtuguinay2000@gmail.com>
2024-01-27 12:02:05 -0800

    post first test commit

--- src/receiver/receiver.cpp
+++ src/receiver/receiver.cpp
@@ -1,23 +1,59 @@
-#include <Arduino.h>
+#include <HardwareSerial.h>
 
-// Pin number for the LED
-const int ledPin = 12;
+HardwareSerial gsmSerial(1);  // Use UART1 on ESP32
 
 void setup() {
-  // Set the LED pin as an output
-  pinMode(ledPin, OUTPUT);
+  // Sync Serial Monitor baud rate with rate set in PlatformIO
+  Serial.begin(9600);
+
+  // GSM module communication, RX on P14, TX on P12
+  gsmSerial.begin(115200, SERIAL_8N1, 14, 12);
+
+  // Check if there is data available from the GSM module
+  if (gsmSerial.available()) {
+    char c = gsmSerial.read();
+    Serial.write(c);  // Print the received character to Serial monitor
+  }
+
+  // Send initial AT command to the GSM module
+  gsmSerial.println("AT");
+  delay(1000);  // Wait for the module to respond
+
+  // Read and print the response from the GSM module. Should receive "+OK".
+  Serial.print("Output of AT command: ");
+  while (gsmSerial.available()) {
+    char responseChar = gsmSerial.read();
+    Serial.write(responseChar);
+  }
+  delay(1000);
+
+  // Send RF Frequency band command to the GSM module. Should receive "+OK".
+  gsmSerial.println("AT+BAND=915000000,M"); // 915Mhz, which is the center frequency for ISM
+  delay(1000);  // Wait for the module to respond
+
+  // Read and print the response from the GSM module
+  Serial.print("Output of AT+BAND command: ");
+  while (gsmSerial.available()) {
+    char responseChar = gsmSerial.read();
+    Serial.write(responseChar);
+  }
+
+  // Send AT+MODE 2 to set to smart receiving power mode. Should receive "+OK".
+  gsmSerial.println("AT+MODE=2");
+  delay(1000);
+
+  // Read and print the response from the GSM module
+  Serial.print("Output of AT+MODE command: ");
+  while (gsmSerial.available()) {
+    char responseChar = gsmSerial.read();
+    Serial.write(responseChar);
+  }
 }
 
 void loop() {
-  // Turn on the LED
-  digitalWrite(ledPin, HIGH);
-  
-  // Wait for 1 second
-  delay(1000);
-  
-  // Turn off the LED
-  digitalWrite(ledPin, LOW);
-  
-  // Wait for 1 second
-  delay(1000);
+  // Read and print the response from the GSM module
+  while (gsmSerial.available()) {
+    char responseChar = gsmSerial.read();
+    Serial.write(responseChar);
+  }
 }
--- src/transmitter/transmitter.cpp
+++ src/transmitter/transmitter.cpp
@@ -1,23 +1,71 @@
-#include <Arduino.h>
+#include <HardwareSerial.h>
 
-// Pin number for the LED
-const int ledPin = 12;
+// Set Button Pin
+const int buttonPin = 27;
+
+HardwareSerial gsmSerial(1);  // Use UART1 on ESP32
 
 void setup() {
-  // Set the LED pin as an output
-  pinMode(ledPin, OUTPUT);
+  // Sync Serial Monitor baud rate with rate set in PlatformIO
+  Serial.begin(9600);
+
+  // Verify pin mode
+  pinMode(buttonPin, INPUT);
+
+  // GSM module communication, RX on P14, TX on P12
+  gsmSerial.begin(115200, SERIAL_8N1, 14, 12);
+
+  // Check if there is data available from the GSM module
+  if (gsmSerial.available()) {
+    char c = gsmSerial.read();
+    Serial.write(c);  // Print the received character to Serial monitor
+  }
+
+  // Send initial AT command to the GSM module
+  gsmSerial.println("AT");
+  delay(1000);  // Wait for the module to respond
+
+  // Read and print the response from the GSM module. Should receive "+OK".
+  Serial.print("Output of AT command: ");
+  while (gsmSerial.available()) {
+    char responseChar = gsmSerial.read();
+    Serial.write(responseChar);
+  }
+  // Print a newline for better readability
+  Serial.println();
+
+  // Wait for 5 seconds before sending the next set of commands
+  delay(5000);
+
+  // Send RF Frequency band command to the GSM module. Should receive "+OK".
+  gsmSerial.println("AT+BAND=915000000,M"); // 915Mhz, which is the center frequency for ISM
+  delay(1000);  // Wait for the module to respond
+
+  // Read and print the response from the GSM module
+  Serial.print("Output of AT+BAND command: ");
+  while (gsmSerial.available()) {
+    char responseChar = gsmSerial.read();
+    Serial.write(responseChar);
+  }
 }
 
 void loop() {
-  // Turn on the LED
-  digitalWrite(ledPin, HIGH);
+  // Read the state of the button pin
+  int buttonState = digitalRead(buttonPin);
 
-  // Wait for 1 second
-  delay(1000);
+  // Check if the button is pressed
+  if (buttonState == HIGH) {
+    Serial.println("Button pressed!");
 
-  // Turn off the LED
-  digitalWrite(ledPin, LOW);
+    // Send message to address 0. Should receive "+OK" from GSM module
+    gsmSerial.println("AT+SEND=0,10,_TEST_TEST");
+    delay(1000);  // Wait for the module to respond
 
-  // Wait for 1 second
-  delay(1000);
+    // Read and print the response from the GSM module
+    Serial.print("Output of AT+SEND command: ");
+    while (gsmSerial.available()) {
+      char responseChar = gsmSerial.read();
+      Serial.write(responseChar);
+    }
+  }
 }
